ccc5032e962bf51a4640dfe27faa6a8b23cc194e
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2022-12-21 21:57:28 +0300

    add about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,6 +9,19 @@ function About() {
             <p className='flex justify-center mt-6 text-gray-700 font-medium'>Here you will find more information about me, what I do, and my current skills mostly in terms </p>
             <p className='flex justify-center text-gray-700 font-medium'>of programming and technology</p>
         </div>
+        <div className='flex gap-5'>
+
+        <div className='ml-16'>
+            <h1 className='text-gray-700 font-bold text-2xl  mt-28'>Get to know me!</h1>
+            <p className='mt-5 text-gray-700 gap-3 text-base'>I'm a <span className='font-bold text-teal-800'>Frontend Web Developer</span> building the Front-end of Websites <br/> and Web Applications that leads to the success of the overall<br/> product. Check out some of my work in the Projects section.</p>
+        </div>
+
+        <div>
+            <h1 className='text-gray-700 font-bold text-2xl ml-16 mt-28 bg-yellow-200'>My Skills</h1>
+            <h2>FRONTEND APPS</h2>
+        </div>
+
+        </div>
     </div>
   )
 }
