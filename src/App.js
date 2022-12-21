bed15fac1101c207dbc042e703d6a6747d0cfd29
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2022-12-21 22:26:07 +0300

    add skills

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Projects/>
       </div>
 
     </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,19 +9,30 @@ function About() {
             <p className='flex justify-center mt-6 text-gray-700 font-medium'>Here you will find more information about me, what I do, and my current skills mostly in terms </p>
             <p className='flex justify-center text-gray-700 font-medium'>of programming and technology</p>
         </div>
-        <div className='flex gap-5'>
+        <div className='flex gap-5 mt-16'>
 
         <div className='ml-16'>
-            <h1 className='text-gray-700 font-bold text-2xl  mt-28'>Get to know me!</h1>
-            <p className='mt-5 text-gray-700 gap-3 text-base'>I'm a <span className='font-bold text-teal-800'>Frontend Web Developer</span> building the Front-end of Websites <br/> and Web Applications that leads to the success of the overall<br/> product. Check out some of my work in the Projects section.</p>
+            <h1 className='text-gray-700 font-bold text-2xl  '>Get to know me!</h1>
+            <p className='mt-5 text-gray-700 gap-3 text-base'>I'm a <span className='font-bold text-teal-800'>Frontend Web Developer</span> building the Front-end<br/> of Websites  and Web Applications that leads to the<br/> success of the overall product. Check out some of<br/> my work in the Projects section.</p>
         </div>
 
-        <div>
-            <h1 className='text-gray-700 font-bold text-2xl ml-16 mt-28 bg-yellow-200'>My Skills</h1>
-            <h2>FRONTEND APPS</h2>
+        <div className='ml-36 '>
+            <h1 className='text-gray-700 font-bold text-2xl'>My Skills</h1>
+            <h2 className='mt-5 font-semibold text-gray-600 text-xl'>FRONTEND APPS</h2>
+            <p className=' text-gray-700 gap-3 text-base mt-3'>Build client-side applications with modern features<br/> like SPA and maintain semantic coding style among<br/> other best practices for SEO optimisation. Use<br/> modern tech such as Nuxt (Vue.js), TailwindCSS, and<br/> GSAP.</p>
+            <h2 className='mt-5 font-semibold text-gray-600 text-xl'>BACKEND APPS</h2>
+            <p className=' text-gray-700 gap-3 text-base mt-3'>Build scalable and maintainable server applications<br/> using modern technology stacks such as Node.js,<br/> Express, and MongoDB</p>
         </div>
 
         </div>
+
+        <div className='flex justify-center items-center mt-16'>
+        <button className='bg-teal-600 w-44 h-16 rounded-full text-white font-semibold hover:bg-gray-400 hover:text-teal-600'>
+            See my Work
+        </button>
+        </div>
+
+        
     </div>
   )
 }
