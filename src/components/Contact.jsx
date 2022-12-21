53cedd4a96862ee7f289c85f0cae67fee4554153
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2022-12-21 23:56:07 +0300

    add contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='mt-40'>
+        <div>
+            <h1 className='flex justify-center font-bold text-5xl text-teal-800'>Send me a message!</h1>
+            <p>Got a question or proposal, or just want</p>
+            <p></p>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
