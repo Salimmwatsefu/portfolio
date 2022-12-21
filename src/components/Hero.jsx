a7a6ac82a842d519abc7719fcd78ad183c5fc703
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2022-12-21 18:38:32 +0300

    Add Navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='mt-16'>Hero</div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { GoProject } from "react-icons/go";
+import { FaUserTie } from "react-icons/fa";
+import { RiArticleFill } from "react-icons/ri";
+import { TfiEmail } from "react-icons/tfi";
+
+function Navbar() {
+  return (
+    <div className='text-gray-600 flex pt-8'>
+        <img src='' alt='Logo' className='ml-5'/>
+
+        <div>
+            <p className='ml-28 font-bold'>
+                <a href='' className='flex'>
+                    <TfiEmail className='text-xl'/>
+                    <span className='ml-1 text-sm'>sjmwatsefu@gmail.com</span>
+                </a>
+            </p>
+        </div>
+        <div className='flex ml-[430px]'>
+            <ul className='flex justify-end gap-6 font-normal'>
+                <li className=''>
+                    <a href='' className='flex'>
+                    <GoProject className='text-xl'/>
+                    <p className='ml-1 '>Projects</p>
+                    </a>
+                </li>
+                <li className='flex'>
+                <a href='' className='flex'>
+                    <FaUserTie className='text-xl'/>
+                    <p >Resume</p>
+                </a>
+                </li>
+                <li className='flex'>
+                <a href='' className='flex'>
+                    <RiArticleFill className='text-xl'/>
+                    <p>Articles</p>
+                </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
