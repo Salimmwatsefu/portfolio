1285e5c7b771de561d91884f03bb25849615c695
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2022-12-21 23:39:17 +0300

    add third project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,24 +14,22 @@ function Projects() {
 
         {/*project 1 */}
      <div className='w-[500px]'>
-     <a href="#" class="group relative block bg-black">
+     <a href="#" class="group relative block bg-black h-72">
   <img
     alt="Travel"
     src="travelhome.png"
-    class="absolute  h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+    class="absolute  h-full w-full object-contain opacity-75 transition-opacity group-hover:opacity-50"
   />
 
   <div class="relative p-8 ">
-    <p class="text-2xl font-bold uppercase tracking-widest text-gray-600">
-      Travelog
-    </p>
 
 
     <div class="mt-20">
       <div
         class="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
       >
-        <p class="text-sm text-white">
+        <h1 className='text-2xl text-white font-bold'>Travelog</h1>
+        <p class="text-sm text-white mt-5">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
           perferendis hic asperiores quibusdam quidem voluptates doloremque
           reiciendis nostrum harum. Repudiandae?
@@ -44,25 +42,21 @@ function Projects() {
 
 {/*project 2 */}
 <div className='w-[500px]'>
-     <a href="#" class="group relative block bg-black">
+     <a href="#" class="group relative block bg-black h-72">
   <img
     alt="Developer"
-    src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-    class="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+    src="electrohome.png"
+    class="absolute  h-full w-full object-contain opacity-75 transition-opacity group-hover:opacity-50"
   />
 
   <div class="relative p-8">
-    <p class="text-sm font-medium uppercase tracking-widest text-pink-500">
-      Developer
-    </p>
-
-    <p class="text-2xl font-bold text-white">Tony Wayne</p>
 
     <div class="mt-20">
       <div
         class="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
       >
-        <p class="text-sm text-white">
+        <h1 className='text-2xl text-white font-bold'>Electrohub</h1>
+        <p class="text-sm text-white mt-5">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
           perferendis hic asperiores quibusdam quidem voluptates doloremque
           reiciendis nostrum harum. Repudiandae?
@@ -75,25 +69,21 @@ function Projects() {
 
 {/*project 3 */}
 <div className='w-[500px]'>
-     <a href="#" class="group relative block bg-black">
+     <a href="#" class="group relative block bg-black h-72">
   <img
     alt="Developer"
-    src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-    class="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+    src="importshome.png"
+    class="absolute inset-0 h-full w-full object-contain opacity-75 transition-opacity group-hover:opacity-50"
   />
 
   <div class="relative p-8">
-    <p class="text-sm font-medium uppercase tracking-widest text-pink-500">
-      Developer
-    </p>
-
-    <p class="text-2xl font-bold text-white">Tony Wayne</p>
 
     <div class="mt-20">
       <div
         class="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
       >
-        <p class="text-sm text-white">
+        <h1 className='text-2xl text-white font-bold'>ImportsBySalim</h1>
+        <p class="text-sm text-white mt-5">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
           perferendis hic asperiores quibusdam quidem voluptates doloremque
           reiciendis nostrum harum. Repudiandae?
