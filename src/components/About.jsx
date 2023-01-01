cd5df63797eb367295e8141fd07d5da3ab38ae18
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2023-01-01 18:34:13 +0300

    text edits

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
         <div>
             <h1 className='flex justify-center text-4xl font-bold text-gray-700'>ABOUT ME</h1>
             <h2 className=' font-bold text-5xl flex justify-center text-teal-600 -mt-7'>_</h2>
-            <p className='flex justify-center mt-6 text-gray-700 font-medium'>Here you will find more information about me, what I do, and my current skills mostly in terms </p>
+            <p className='flex justify-center mt-6 text-gray-700 font-medium text-center'>Here you will find more information about me, what I do, and my current skills mostly in terms </p>
             <p className='flex justify-center text-gray-700 font-medium'>of programming and technology</p>
         </div>
         {/*<div className='flex gap-5 mt-16'>
